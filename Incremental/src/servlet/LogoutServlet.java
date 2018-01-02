885cae73c18d10ee4137572026ab789a141670e6
stefano.quaglietti <stefano.quaglietti@QUAGLIETTI.methodo.local>
2018-01-02 18:06:07 +0100

    Logout servlet

---
+++ Incremental/src/servlet/LogoutServlet.java
@@ -0,0 +1,19 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class LogoutServlet extends BaseServlet {
+
+	@Override
+	protected void performTask(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.getSession().invalidate();		
+		toRedirectPage(request, response, "home");
+	
+	}
+}
